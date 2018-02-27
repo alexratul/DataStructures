7d7fdd723275eca26f50cb7f1785c79671f1a60e
alexratul <alexratul12@outlook.com>
2018-02-26 20:16:31 -0800

    Add files to the branch

---
+++ src/main/java/dataStructures/Nodes/listNodes/node.java
@@ -0,0 +1,38 @@
+package dataStructures.Nodes.listNodes;
+
+public class node {
+
+    private Object data;
+    private node next;
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+
+    public node getNext() {
+        return next;
+    }
+
+    public void setNext(node next) {
+        this.next = next;
+    }
+
+
+    public node()
+    {
+        data = null;
+        next = null;
+
+    }
+
+    public node(Object input, node nextNode)
+    {
+        setData(input);
+        setNext(nextNode);
+    }
+
+}
---
+++ src/main/java/dataStructures/Structures/lists/simpleList.java
@@ -0,0 +1,16 @@
+package dataStructures.Structures.lists;
+import dataStructures.Nodes.listNodes.node;
+
+
+public class simpleList {
+
+    private node current;
+    private int lenght = 0;
+
+
+    public simpleList()
+    {
+        current = new node();
+        lenght = 1;
+    }
+}
